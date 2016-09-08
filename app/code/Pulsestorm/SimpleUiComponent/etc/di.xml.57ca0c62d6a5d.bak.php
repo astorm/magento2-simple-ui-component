224387c7635db567d717b2defb8c2ecf00c0c446
Alan Storm <astorm@alanstorm.com>
2016-09-07 20:21:46 -0700

    removing bak files

--- app/code/Pulsestorm/SimpleUiComponent/etc/di.xml.57ca0c62d6a5d.bak.php
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0"?>
-<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:ObjectManager/etc/config.xsd">
-    <type name="Magento\Framework\Config\Dom">
-        <plugin name="pulsestorm_simpleuicomponent_magento_framework_config_dom" type="Pulsestorm\SimpleUiComponent\Plugin\Magento\Framework\Config\Dom"/>
-    </type>
-</config>
--- app/code/Pulsestorm/SimpleUiComponent/etc/di.xml.57cb2adaed040.bak.php
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0"?>
-<config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:framework:ObjectManager/etc/config.xsd">
-    <type name="Magento\Framework\Config\Dom">
-        <plugin name="pulsestorm_simpleuicomponent_magento_framework_config_dom" type="Pulsestorm\SimpleUiComponent\Plugin\Magento\Framework\Config\Dom"/>
-    </type>
-    <type name="Magento\Framework\View\Element\UiComponent\Config\DomMerger">
-        <plugin name="pulsestorm_simpleuicomponent_magento_framework_view_element_uicomponent_config_dommerger" type="Pulsestorm\SimpleUiComponent\Plugin\Magento\Framework\View\Element\UiComponent\Config\DomMerger"/>
-    </type>
-</config>
