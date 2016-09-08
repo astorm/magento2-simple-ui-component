b3a0768c54b1f429427eb6126db142b4518a12a7
Alan Storm <astorm@alanstorm.com>
2016-09-08 09:32:56 -0700

    Removing uneeded plugin stuff.

--- app/code/Pulsestorm/SimpleUiComponent/Plugin/Magento/Framework/Config/ValidationStateInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Pulsestorm\SimpleUiComponent\Plugin\Magento\Framework\Config;
-class ValidationStateInterface
-{
-    public function afterIsValidationRequired($subject, $result)
-    {
-        return false;
-    }
-}
