2310ad3472e8900b5ee7f5468c28698066c86c81
Alan Storm <astorm@alanstorm.com>
2016-09-08 09:37:36 -0700

    Adding warning.

--- app/code/Pulsestorm/SimpleUiComponent/Model/ValidationState.php
+++ app/code/Pulsestorm/SimpleUiComponent/Model/ValidationState.php
@@ -3,7 +3,11 @@ namespace Pulsestorm\SimpleUiComponent\Model;
 class ValidationState extends \Magento\Framework\App\Arguments\ValidationState
 {
     public function isValidationRequired()
-    {        
+    {     
+        var_dump('Hey there, this extension disabled XSD validation in ' . __FILE__ . 
+        'because there\'s no other way to add a new component definition to ' . 
+        'the merged definition.xml.  You probably don\'t want to use this in ' .
+        'a real system.  EDUCATION ONLY, ETC.'  );   
         return false;
     }
 }
